432d7c98b70163ab27f6be2a5ff00b34dde4d591
chandramouli <chandramoulichandru9741@gmail.com>
2023-09-07 10:01:22 +0530

    created SingleCard component and added some styles

---
+++ src/components/SingleCard.js
@@ -0,0 +1,11 @@
+import "./SingleCard.css";
+
+export default function SingleCard({ card }){
+    return(
+        <div className="card">
+            <img className="front" src={card.src} alt="card front" />
+            <img className="back" src="/img/cover.png" alt="card back" />
+          </div>
+    );
+}
+
